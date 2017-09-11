370cca407614a3f77de4e06d945fb2b167d0e188
DESKTOP-BC41I7N\SB <ksb304@naver.com>
2017-09-11 12:12:59 +0900

    awd

--- webCrawling/src/main/java/kr/co/web/filter/CORSFilter.java
+++ webCrawling/src/main/java/kr/co/web/filter/CORSFilter.java
@@ -15,6 +15,8 @@ public class CORSFilter implements Filter{
 	 @Override
 	    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 	       
+		 	System.out.println("asdasdsadasd");
+		 
 	        HttpServletResponse response = (HttpServletResponse) res;
 	        response.setHeader("Access-Control-Allow-Origin", "*");
 	        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
